765099cafcfe066b74c1ac3a3ca6ff3f8f9708bc
Hatoridev <jonasdev21@outlook.com>
2024-02-14 11:20:30 -0300

    Quinta aula de Python básico

---
+++ ExFuncao.py
@@ -0,0 +1,25 @@
+def message1():
+    print("Hello World")
+
+def message2():
+    return 'Hello World'
+
+message1()
+
+texto = message2()
+print(texto)
+
+# Explicação do código:
+
+# - Definimos duas funções, message1() e message2().
+
+# - A função message1() imprime "Hello World" na tela.
+
+# - A função message2() retorna a string 'Hello World'.
+
+# - Chamamos a função message1(), que imprime "Hello World".
+
+# - Chamamos a função message2() e armazenamos o resultado retornado em uma variável chamada texto.
+
+# - Em seguida, imprimimos o valor da variável texto, que contém a string 'Hello World'.
+
