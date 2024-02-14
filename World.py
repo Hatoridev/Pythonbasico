093fdde66bae7a5294a6ffb84b7525203f80704c
Hatoridev <jonasdev21@outlook.com>
2024-02-14 10:37:14 -0300

    Segunda aula de Python básico

---
+++ World.py
@@ -0,0 +1,16 @@
+def imprimir_hello():  
+    return "Hello, World!"  
+
+resultado = imprimir_hello()  
+print(resultado)  
+
+#Este foi um exemplo básico em Python para imprimir "Hello, World!"
+
+# Aqui está o que cada parte do código faz:
+
+# - Definimos uma função chamada imprimir_hello que retorna a mensagem "Hello, World!"
+
+# - Chamamos a função e armazenamos o resultado na variável resultado
+
+# - Imprimimos o resultado na tela
+
---
+++ script.py
@@ -0,0 +1,39 @@
+def conversa():
+    print("Lucas: Olá meu amigo, tudo bem contigo?")
+    input("Pressione Enter para continuar...")
+
+    print("João: Tudo ótimo, Lucas.")
+    input("Pressione Enter para continuar...")
+
+    print("João: Que horas são aí, Lucas?")
+    input("Pressione Enter para continuar...")
+
+    print("Lucas: 18:00h")
+    input("Pressione Enter para prosseguir...")
+
+    conversa()
+
+def ligacao():
+    print("Geovana: Alô?")
+    input("Pressione Enter para escutar...")
+
+    print("Amanda: Amiga, você já assistiu Pânico 6?")
+    input("Pressione Enter para escutar...")
+
+    print("Geovana: Já sim amiga... Vamos assistir juntas?")
+    input("Pressione Enter para escutar...")
+
+    print("Amanda: Claro, vamos sim!")
+
+ligacao()
+
+# Explicação do código:
+
+# - Definimos duas funções, conversa() e ligacao(), que simulam diálogos entre pessoas.
+
+# - Dentro de cada função, imprimimos mensagens e solicitamos ao usuário que pressione Enter para continuar.
+
+# - Depois de imprimir os diálogos, chamamos a função conversa() para iniciar o diálogo entre Lucas e João.
+
+# - Finalmente, chamamos a função ligacao() para simular uma ligação entre Geovana e Amanda.
+
